46401cadb291adf5168fb398d5656d627b7efdb8
이주영 <141599648+ljy1348@users.noreply.github.com>
2024-10-11 00:44:02 +0900

    프로젝트 기본 구성

---
+++ src/main/java/com/example/hhp_ecommerce/HhpEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhp_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhpEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhpEcommerceApplication.class, args);
+    }
+
+}
